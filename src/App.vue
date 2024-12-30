e41e0c9d429f328939016a1d8c8da4946553713d
Digs-LS <diego.lucas.sousa@live.com>
2024-12-30 10:24:18 -0300

    feat: persistência do nome do usuário, e novo input para adicionar tarefas

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,25 @@
 <script setup>
-import { ref, computed } from 'vue';
+import { ref, computed, watch, onMounted } from 'vue';
 
 const nome = ref('')
 const lista_tarefas = ref([])
 
 const conteudo_input = ref('')
+const adicionarTarefa = () => {
+  if (conteudo_input.value.trim() === '') return
+  lista_tarefas.value.push({
+    id: Date.now(),
+    conteudo: conteudo_input.value,
+    created_at: Date.now()
+  })
+  conteudo_input.value = ''
+}
 const lista_tarefas_asc = computed(() => lista_tarefas.value.sort((a, b) => {
   return b.created_at - a.created_at
 })) // Ordena a lista de tarefas em ordem de criação (ascendente)
+
+watch(nome, (newval) => {localStorage.setItem('nome', newval)})
+onMounted(() => {nome.value = localStorage.getItem('nome') || ''})
 </script>
 
 <template>
@@ -19,6 +31,19 @@ const lista_tarefas_asc = computed(() => lista_tarefas.value.sort((a, b) => {
       </h2>
     </section>
   </main>
+
+  <section class="criar-tarefa">
+    <h3>Criar tarefa</h3>
+    <form @sumbimt.prevent="adicionarTarefa" class="action">
+      <h4>Conteúdo</h4>
+      <input
+        type="text"
+        placeholder="por exemplo: Comprar pão"
+        v-model="conteudo_input"
+      />
+    </form>
+
+  </section>
 
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import './main.css'
+import './main.css'// Importa o arquivo main.css como a fonte de estilização global
 import App from './App.vue'
 
 createApp(App).mount('#app')
